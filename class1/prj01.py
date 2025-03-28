a58e7f70f2a4bd9f841c9b622e57f55bfb6ecdfb
eric20150705 <chiangeric0705@gmail.com>
2025-03-28 20:32:54 +0800

    feat: 新增 Pygame 初始化及視窗建立功能

--- class1/prj01.py
+++ class1/prj01.py
@@ -1,1 +1,17 @@
-print("hello world")
+######################匯入模組######################
+import pygame
+import sys
+
+######################初始化######################
+pygame.init()
+width = 640
+height = 320
+######################建立視窗及物件######################
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("my first game")
+######################循環偵測######################
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
